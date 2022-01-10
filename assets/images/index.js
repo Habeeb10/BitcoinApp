6e3f225d9add5db3a73c982181431221c726386c
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2022-01-10 14:49:29 +0100

    feat: added png images

---
+++ assets/images/index.js
@@ -0,0 +1,6 @@
+export const Band = require('./Band.png');
+export const Bitcoin = require('./Bitcoin.png');
+export const Cardano = require('./Cardano.png');
+export const Ethereum = require('./Ethereum.png');
+export const Tether = require('./Tether.png');
+export const Tron = require('./Tron.png');
