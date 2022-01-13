d78236d04701218b642aab6f015069f7c2d5bd41
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2022-01-13 22:07:52 +0100

    refactor

---
+++ src/screens/market/styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../common/colors';
+import {hp, wp} from '../../common/utils';
+
+export const MarketStyles = StyleSheet.create({
+  welcomecontainer: {
+    flexDirection: 'row',
+  },
+});
